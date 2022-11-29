4213522dad592edbc163ba22a95d3022b4b6a2b2
Seth Strouf <seth.strouf@doane.edu>
2022-11-28 20:06:52 -0600

    Update 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
 
 const NotFound = () => {
 
@@ -7,7 +8,32 @@ const NotFound = () => {
   }, [])
 
   return (
-    <h1 className='text-center'>Not Found</h1>
+    <>
+      <div className="min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+        <div className="mx-auto max-w-max">
+          <main className="sm:flex">
+            <p className="text-4xl font-bold tracking-tight text-sky-600 sm:text-5xl">404</p>
+            <div className="sm:ml-6">
+              <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+                <h1 className="text-4xl font-bold tracking-tight text-gray-700 sm:text-5xl">Page not found</h1>
+                <p className="mt-1 text-base text-gray-500">Please check the URL in the address bar and try again.</p>
+              </div>
+              <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+                <NavLink to={'/'} className="inline-flex items-center rounded-md border border-transparent bg-sky-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2">
+                  Go back home
+                </NavLink>
+                <a
+                  href="#"
+                  className="inline-flex items-center rounded-md border border-transparent bg-sky-100 px-4 py-2 text-sm font-medium text-sky-700 hover:bg-sky-200 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2"
+                >
+                  Contact support
+                </a>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
   )
 }
 
