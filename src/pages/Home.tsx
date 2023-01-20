032bc78fcca6b25a5a0e8f04b88305d11574ccac
Seth Strouf <seth.strouf@doane.edu>
2023-01-20 11:22:03 -0600

    feat(home): update home tag

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,9 +7,14 @@ const Home = () => {
   }, [])
 
   return (
-    <>
-      <p className='text-center p-24 text-5xl sm:text-7xl lg:text-9xl leading-normal'>Tagline for <span className='cursive text-sky-700 underline font-bold'>Songsemble</span> here</p>
-    </>
+  <div className='text-left p-24 font-light text-4xl sm:text-6xl lg:text-8xl leading-normal'>
+    <p>
+      <span className='cursive text-sky-600 font-bold text-5xl sm:text-7xl lg:text-9xl'>Songsemble...</span>
+      <br />
+      your music <span className="font-normal text-sky-800 ">library </span>
+      and <span className="font-normal text-sky-800 ">networking</span> solution.
+    </p>
+  </div>
   )
 }
 
